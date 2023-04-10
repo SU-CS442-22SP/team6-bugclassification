219a4d12b02f7c0e92f667c3c57d537b529a08f4
Mustafa Ahmet Yücel <a.ahmety@icloud.com>
2023-04-11 00:34:18 +0300

    Client UI initialized
    Initial design of client UI implemented
    
    File picking is integrated, client now picks java files

--- client/client.py
+++ client/client.py
@@ -1,5 +1,17 @@
-import socket
+import socket, sys
 import variables as gv
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QFileDialog,
+    QScrollArea,
+)
 
 
 def main():
@@ -11,5 +23,51 @@ def main():
     client_socket.close()
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.load_client_ui()
+
+    def load_client_ui(self):
+        uic.loadUi(gv.UI_PATH, self)
+        self.select_button = self.findChild(QPushButton, "select_file_button")
+        self.select_button.clicked.connect(self._pick_file)
+
+        self.remove_button = self.findChild(QPushButton, "deselect_file_button")
+        self.remove_button.clicked.connect(self._remove_picked_file)
+
+        self.path_label = self.findChild(QLabel, "file_path_label")
+
+        self.classify_button = self.findChild(QPushButton, "classify_button")
+        self.classify_button.clicked.connect(self._send_to_server)
+
+        self.output_scrollarea = self.findChild(QScrollArea, "output_scrollArea")
+
+
+    def _pick_file(self):
+        fname = QFileDialog.getOpenFileName(self, 'Choose', gv.BASE_DIR,"Java files (*.java)")[0]
+        if fname != '':
+            self.path_label.setText(fname)
+            self.remove_button.setEnabled(True)
+            self.classify_button.setEnabled(True)
+
+
+    def _remove_picked_file(self):
+        self.path_label.setText("None selected")
+        self.remove_button.setDisabled(True)
+        self.classify_button.setDisabled(True)
+
+
+    def _send_to_server(self):
+        pass
+
+
+    def print_result(self, result):
+        pass
+
+
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
--- client/variables.py
+++ client/variables.py
@@ -1,2 +1,6 @@
+import os
+BASE_DIR = os.path.dirname(__file__)
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+UI_PATH = os.path.join(ASSETS_DIR, "client.ui")
 IP_ADDRESS = "0.0.0.0"
 PORT = 8000
