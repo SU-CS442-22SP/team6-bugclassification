7c04e98b3b9c44993ba64ce53c985609d77ddd64
Mustafa Ahmet Yücel <a.ahmety@icloud.com>
2023-04-10 21:15:28 +0300

    minor refactor for both the client and server

--- client/client.py
+++ client/client.py
@@ -1,11 +1,15 @@
 import socket
+import variables as gv
 
-IP_ADDRESS = "192.168.0.103"
-PORT = 8000
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP_ADDRESS, PORT))
-client_socket.send(b"Hello from the client!")
-response = client_socket.recv(1024)
-print(response.decode("utf-8"))
-client_socket.close()
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((gv.IP_ADDRESS, gv.PORT))
+    client_socket.send(b"Hello from the client!")
+    response = client_socket.recv(1024)
+    print(response.decode("utf-8"))
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
