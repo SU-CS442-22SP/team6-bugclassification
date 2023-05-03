7f3408d88a0731011b67a0ea854a9ab4eadb1534
Yasin Ughur <yasinugur.cs@gmail.com>
2023-05-03 11:45:10 +0300

    :bug: ChatGPT fix

--- server/chatgpt.py
+++ server/chatgpt.py
@@ -1,6 +1,6 @@
 import openai
-from server.bugclassification_base import BugClassificationBase
-from server.config import settings
+from bugclassification_base import BugClassificationBase
+from config import settings
 
 openai.api_key = settings.CHATGPT_KEY
 
@@ -10,7 +10,12 @@ class ChatGPTService(BugClassificationBase):
         self.messages = self._start_chat()
 
     def _start_chat(self):
-        messages = [{"role": "user", "content": settings.CHATGPT_START_MESSAGE}]
+        messages = [
+            {
+                "role": "system",
+                "content": settings.CHATGPT_START_MESSAGE + "\n" + settings.BUGS_LIST,
+            }
+        ]
         return messages
 
     def classify(self, buggged_code):
